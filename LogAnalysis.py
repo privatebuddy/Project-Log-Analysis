0bab250018bf13b2c4b2c6b3703f43aad8fb505c
Sasin Siriskaowkul <privatebuddy@gmail.com>
2018-09-07 16:29:00 +0700

    Fix Requirement 2

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -3,18 +3,30 @@
 import psycopg2
 
 DB_NAME = "news"
-SQL_QUERY_1 = """SELECT path,COUNT(*) as total 
-FROM log JOIN articles ON path = format('/article/'||articles.slug) 
-GROUP BY path ORDER BY total DESC LIMIT 3"""
-SQL_QUERY_2 = """SELECT name,COUNT(*) AS total 
-FROM authors JOIN articles ON author = authors.id JOIN log ON path = format('/article/'||articles.slug) 
+SQL_QUERY_1 = """SELECT title, views
+    FROM articles
+    INNER JOIN
+        (SELECT path, count(path) AS views
+         FROM log
+         GROUP BY log.path) AS log
+    ON log.path = '/article/' || articles.slug
+    ORDER BY views DESC
+    LIMIT 3;"""
+SQL_QUERY_2 = """SELECT name,COUNT(*) AS total
+FROM authors JOIN articles ON author = authors.id
+JOIN log ON path = format('/article/'||articles.slug)
 GROUP BY name ORDER BY total DESC"""
-SQL_QUERY_3_SUCCESS = """SELECT date(time) as Date,count(*) as SuccessTime 
-FROM log where status = '200 OK' 
-GROUP BY Date ORDER BY SuccessTime desc"""
-SQL_QUERY_3_FAIL = """SELECT date(time) as Date,count(*) as ErrorTime 
-FROM log where status = '404 NOT FOUND' 
-GROUP BY Date ORDER BY ErrorTime desc"""
+SQL_QUERY_3 = """SELECT
+    date(time) as Date,
+    Count(CASE status when '200 OK' then 1 else null end) as SuccessTime,
+    Count(CASE status when '404 NOT FOUND' then 1 else null end) as FailTime,
+    (COALESCE(Count(CASE status when '404 NOT FOUND' then 1 else null end),0)
+     *100)/ Count(CASE status when '200 OK' then 1 else null end)
+     as ErrorPercentage
+from
+log
+GROUP BY
+Date ORDER BY ErrorPercentage desc  """
 
 
 def get_data_from_query(query, database_name):
@@ -27,45 +39,39 @@ def get_data_from_query(query, database_name):
 
 
 def question_one():
-    print ("Question 1")
+    report_string = "Question 1 \n"
     for data in get_data_from_query(SQL_QUERY_1, DB_NAME):
-        print('{0} {2} {1} {3}'.format(data[0].replace("/article/", "").replace("-", " ")
-                                       , data[1]
-                                       , ":"
-                                       , "views")
-              )
-
-    return "======"
+        report_string += '{0} {2} {1} {3} \n'.format(data[0].replace("/article/", "").replace("-", " "), data[1], ":", "views")
+    return report_string
 
 
 def question_two():
-    print ("Question 2")
+    report_string="Question 2 \n"
     for data in get_data_from_query(SQL_QUERY_2, DB_NAME):
-        print('{0} {1} {2} {3}'.format(data[0], ":", data[1], "views"))
+        report_string += '{0} {1} {2} {3}'.format(data[0], ":", data[1], "views \n")
 
-    return "======"
+    return report_string
 
 
 def question_three():
-    print ("Question 3");
-    for good_data in get_data_from_query(SQL_QUERY_3_SUCCESS, DB_NAME):
-        for bad_data in get_data_from_query(SQL_QUERY_3_FAIL, DB_NAME):
-            if good_data[0] == bad_data[0]:
-                bad_traffic_percent = (bad_data[1]*100)/good_data[1]
-                if bad_traffic_percent > 1:
-                    print('{0} {1} {2} {3} {4}'.format("Date:"
-                                                       , good_data[0]
-                                                       , "has bad Traffic of "
-                                                       , bad_traffic_percent
-                                                       , "%"))
+    report_string = "Question 3 \n"
+    for data in get_data_from_query(SQL_QUERY_3, DB_NAME):
+        if data[3] > 1:
+            report_string += '{0} {1} {2} {3} {4}'.format("Date:", data[0], "has bad Traffic of ", data[3], "% \n")
 
-    return "======"
+    return report_string
 
 
 def get_all_answer():
-    print(question_one())
-    print(question_two())
-    return question_three()
+    f = open('Report.txt', 'w')
+    f.write(str(question_one()))
+    f.write(str(question_two()))
+    f.write(str(question_three()))
+    f.close()
+    return ""
 
 
-print(get_all_answer())
+if __name__ == '__main__':
+    get_all_answer()
+else:
+    print('I am being imported from another module')
